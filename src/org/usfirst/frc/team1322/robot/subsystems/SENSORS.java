47ba783656a313d2e4cc4a245f79d54e6331b38d
Robert Williams <rufusbobbybills@gmail.com>
2018-02-19 02:58:25 -0500

    Added Primitive Speed Conversion Calculations

--- src/org/usfirst/frc/team1322/robot/subsystems/SENSORS.java
+++ src/org/usfirst/frc/team1322/robot/subsystems/SENSORS.java
@@ -101,90 +101,151 @@ public class SENSORS extends Subsystem {
     }
 
 
+  /*****************************************************************/
+  /* Wheel Speed / Motor Encoder Conversion Calculations           */
+  /*****************************************************************/
 	
-	
-	
-	/*****************************************************************/
-	/* UltraSonic Distance Measurement Conversion Calculations       */
-	/*****************************************************************/
-	
-	/** Method: calcDistUSRear - Calculates the Distance Sensed by the
-	 *  Rear Side UltraSonic Sensor.
-     *  @param: Sensor Sensed Voltage
-     *  @return: Sensor Sensed Distance */
-	private float calcDistUSRear(float VoltSnsd)
-	  {
-      float V2DConvFx; // Voltage to Distance Conversion Factor (inch/volt)
-      float DistOfst;  // Distance Offset from Edge of Robot, inboard is positive (inch)
-      float DistCalcd; // Calculated Distance (inches)
-      
-      V2DConvFx = K_SensorCal.KUSS_Fx_VoltToDistRear;   // inch/volt
-      DistOfst = K_SensorCal.KUSS_l_DistOfstRear;       // inch
-
-      DistCalcd = calcDistUSSensor(VoltSnsd, V2DConvFx, DistOfst);
-      
-      return (DistCalcd);
-      }
 
 	
-	/** Method: calcDistUSLeft - Calculates the Distance Sensed by the
-	 *  Left Side UltraSonic Sensor.
-     *  @param: Sensor Sensed Voltage
-     *  @return: Sensor Sensed Distance */
-	private float calcDistUSLeft(float VoltSnsd)
-	  {
-      float V2DConvFx; // Voltage to Distance Conversion Factor (inch/volt)
-      float DistOfst;  // Distance Offset from Edge of Robot, inboard is positive (inch)
-      float DistCalcd; // Calculated Distance (inches)
-      
-      V2DConvFx = K_SensorCal.KUSS_Fx_VoltToDistLeft;   // inch/volt
-      DistOfst = K_SensorCal.KUSS_l_DistOfstLeft;       // inch
+	  /** Method: calcRPMEncoder - Calculates the Rotational
+	    *  speed of an Encoder Shaft from the number of Encoder
+	    *  Counts in the sample and the Sample Time.  Result
+	    *  is in rpm.
+	    *  @param1: Sample Counts (counts)
+	    *  @param2: Sensor Period (seconds)
+	    *  @return: Encoder Shaft Speed (rpm) */	
+	  private float calcRPMEncoder(int SmplCnts,
+	  	                           float SmplPeriod)
+	    {
+	    float SmplCntsPerSec;
+	    float SpdEncoder;
 
-      DistCalcd = calcDistUSSensor(VoltSnsd, V2DConvFx, DistOfst);
+	    if (SmplPeriod > (float)0.0)
+	      {
+	      SmplCntsPerSec = (float)(((float)SmplCnts)/SmplPeriod);
+	      SpdEncoder =  (float)(SmplCntsPerSec/K_SensorCal.KWSS_Cnt_PulsePerRevEncoder)*((float)60);
+	      }
+	    else if (SmplCnts > (int) 0)
+	      {
+	      SpdEncoder = (float)10000.0;	
+	      }
+	    else
+	      {
+	      SpdEncoder = (float)0.0;
+	      }
+	      
+	    return SpdEncoder;
+		}
+
+	  
+	  /** Method: calcRPMWheel - Calculates the Rotational
+	    *  speed of a Wheel Shaft from the Encoder Shaft
+	    *  Rotational Speed.  Result is in rpm.
+	    *  @param: Encoder Shaft Speed (rpm)
+	    *  @return: Wheel Shaft Speed  (rpm) */	
+	  private float calcRPMWheel(float SpdEncoder)
+	    {
+	    return ((float)(K_SensorCal.KWSS_r_EncoderToWheel * SpdEncoder));
+		}
+
+	  
+	  /** Method: calcLinealSpd - Calculates the Linear 
+	    *  Speed of the Robot from the Average Wheel Speed
+	    *  when moving directly forward or rearward.
+	    *  @param: Average Wheel Shaft Speed (rpm)
+	    *  @return: Robot Linear Speed (fps) */	
+	  private float calcLinealSpd(float SpdWhl)
+	    {
+	    return ((float)((K_SensorCal.KWSS_l_DistPerRevWheel * SpdWhl)/(float)60));
+		}
+	  
+	  
+	  
+	  
+	  
+	  
+	
+  /*****************************************************************/
+  /* UltraSonic Distance Measurement Conversion Calculations       */
+  /*****************************************************************/
+	
+  /** Method: calcDistUSRear - Calculates the Distance Sensed by the
+	*  Rear Side UltraSonic Sensor.
+    *  @param: Sensor Sensed Voltage
+    *  @return: Sensor Sensed Distance */
+  private float calcDistUSRear(float VoltSnsd)
+	{
+    float V2DConvFx; // Voltage to Distance Conversion Factor (inch/volt)
+    float DistOfst;  // Distance Offset from Edge of Robot, inboard is positive (inch)
+    float DistCalcd; // Calculated Distance (inches)
       
-      return (DistCalcd);
-      }
+    V2DConvFx = K_SensorCal.KUSS_Fx_VoltToDistRear;   // inch/volt
+    DistOfst = K_SensorCal.KUSS_l_DistOfstRear;       // inch
+
+    DistCalcd = calcDistUSSensor(VoltSnsd, V2DConvFx, DistOfst);
+      
+    return (DistCalcd);
+    }
+
+
+  /** Method: calcDistUSLeft - Calculates the Distance Sensed by the
+	*  Left Side UltraSonic Sensor.
+    *  @param: Sensor Sensed Voltage
+    *  @return: Sensor Sensed Distance */
+  private float calcDistUSLeft(float VoltSnsd)
+    {
+    float V2DConvFx; // Voltage to Distance Conversion Factor (inch/volt)
+    float DistOfst;  // Distance Offset from Edge of Robot, inboard is positive (inch)
+    float DistCalcd; // Calculated Distance (inches)
+      
+    V2DConvFx = K_SensorCal.KUSS_Fx_VoltToDistLeft;   // inch/volt
+    DistOfst = K_SensorCal.KUSS_l_DistOfstLeft;       // inch
+
+    DistCalcd = calcDistUSSensor(VoltSnsd, V2DConvFx, DistOfst);
+      
+    return (DistCalcd);
+    }
 
 	
-	/** Method: calcDistUSRight - Calculates the Distance Sensed by the
-	 *  Right Side UltraSonic Sensor.
-     *  @param: Sensor Sensed Voltage
-     *  @return: Sensor Sensed Distance */
-	private float calcDistUSRight(float VoltSnsd)
-	  {
-      float V2DConvFx; // Voltage to Distance Conversion Factor (inch/volt)
-      float DistOfst;  // Distance Offset from Edge of Robot, inboard is positive (inch)
-      float DistCalcd; // Calculated Distance (inches)
+  /** Method: calcDistUSRight - Calculates the Distance Sensed by the
+    *  Right Side UltraSonic Sensor.
+    *  @param: Sensor Sensed Voltage
+    *  @return: Sensor Sensed Distance */
+  private float calcDistUSRight(float VoltSnsd)
+  	{
+    float V2DConvFx; // Voltage to Distance Conversion Factor (inch/volt)
+    float DistOfst;  // Distance Offset from Edge of Robot, inboard is positive (inch)
+    float DistCalcd; // Calculated Distance (inches)
       
-      V2DConvFx = K_SensorCal.KUSS_Fx_VoltToDistRight;  // inch/volt
-      DistOfst = K_SensorCal.KUSS_l_DistOfstRight;      // inch
+    V2DConvFx = K_SensorCal.KUSS_Fx_VoltToDistRight;  // inch/volt
+    DistOfst = K_SensorCal.KUSS_l_DistOfstRight;      // inch
 
-      DistCalcd = calcDistUSSensor(VoltSnsd, V2DConvFx, DistOfst);
+    DistCalcd = calcDistUSSensor(VoltSnsd, V2DConvFx, DistOfst);
       
-      return (DistCalcd);
-      }
+    return (DistCalcd);
+    }
 
 	
-	/** Method: calcDistUSSensor - Calculates the Distance Sensed
-	 * by UltraSonic Sensors from the Voltage to Distance Conversion
-	 * Factor and the Distance Offset that the Sensor is in-board
-	 * from the edge of the robot.
+  /** Method: calcDistUSSensor - Calculates the Distance Sensed
+    * by UltraSonic Sensors from the Voltage to Distance Conversion
+    * Factor and the Distance Offset that the Sensor is in-board
+    * from the edge of the robot.
     *  @param1: Sensor Sensed Voltage
     *  @param2: Sensor Voltage to Distance Conversion Factor
     *  @param3: Sensor Distance Offset from Edge of Robot 
     *  @return: Sensor Sensed Distance */	
-	private float calcDistUSSensor(float SnsdVolt,
-			                       float V2DConvFx,
-			                       float DistOfst) {
-
-      float SnsdDist; // Sensed Distance     
+  private float calcDistUSSensor(float SnsdVolt,
+  	                             float V2DConvFx,
+                                 float DistOfst)
+    {
+    float SnsdDist; // Sensed Distance     
       
-      SnsdDist = (float)((SnsdVolt * V2DConvFx) - DistOfst);
-      if (SnsdDist < (float)0.0) SnsdDist = (float)0.0; 
+    SnsdDist = (float)((SnsdVolt * V2DConvFx) - DistOfst);
+    if (SnsdDist < (float)0.0) SnsdDist = (float)0.0; 
       
-      return SnsdDist;
+    return SnsdDist;
 	}
-	
-	
+
+  
 }
 
