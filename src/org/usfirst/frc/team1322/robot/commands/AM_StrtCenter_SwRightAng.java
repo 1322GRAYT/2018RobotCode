f46fb85713e6f3158f09af3b42a541deffad03d5
Robert Williams <rufusbobbybills@gmail.com>
2018-04-12 14:44:04 -0400

    Increased Angle and Dist for Center/Angled Auto Cmnd Program

--- src/org/usfirst/frc/team1322/robot/commands/AM_StrtCenter_SwRightAng.java
+++ src/org/usfirst/frc/team1322/robot/commands/AM_StrtCenter_SwRightAng.java
@@ -26,12 +26,12 @@ public class AM_StrtCenter_SwRightAng extends CommandGroup {
     	addParallel(new AC_ResetEncoders(K_CmndCal.KCMD_t_EncdrRstDly));    	
     	addSequential(new BM_LiftHoldPstn());  
     	addSequential(new AC_DriveEncdrByDist((float)1.0, (float)0.3, (float)0.25, (float)0.10, (float)0.0, true, true));
-    	addSequential(new AC_TurnByGyro(0.7, 38.6, true)); // Turn CW to 38.6 deg
+    	addSequential(new AC_TurnByGyro(0.9, 41.0, true)); // Turn CW to 38.6 deg
 		addParallel(new AC_TimeDelay(K_CmndCal.KCMD_t_PostMoveDly));
     	addSequential(new BM_LiftHoldPstn());  
     	addParallel(new AC_ResetEncoders(K_CmndCal.KCMD_t_EncdrRstDly));    	
     	addSequential(new BM_LiftHoldPstn());  
-    	addSequential(new AC_DriveEncdrByDist((float)7.35, (float)0.8, (float)1.0, (float)0.10, (float)38.6, true, true));
+    	addSequential(new AC_DriveEncdrByDist((float)8.27, (float)0.8, (float)1.0, (float)0.10, (float)41.0, true, true));
 	    addSequential(new AC_TurnByGyro(-0.9, 0.0, true)); // Turn CCW to 0 deg
     	addSequential(new BM_LiftRaiseToMid());
 	    addSequential(new AC_DriveByGyroTime(0.0, 0.5, 0.5, true));
@@ -39,7 +39,7 @@ public class AM_StrtCenter_SwRightAng extends CommandGroup {
 	    addSequential(new BM_ClawOpen()); 
 		addParallel(new AC_TimeDelay((float)0.5));
     	addSequential(new BM_LiftHoldPstn());
-    	addSequential(new AC_DriveByGyroTime(0.5, 0.0, 2.0, false));  // Strafe to the Right    		
+    	addSequential(new AC_DriveByGyroTime(0.7, 0.0, 2.5, false));  // Strafe to the Right    		
     	addSequential(new AC_TimeDelay(K_CmndCal.KCMD_t_PostMoveDly));
     	addSequential(new AC_ResetEncoders(K_CmndCal.KCMD_t_EncdrRstDly));
     	addSequential(new AC_DriveEncdrByDist((float)4.0, (float)0.5, (float)0.5, (float)0.1, (float)0.0, true, false));
